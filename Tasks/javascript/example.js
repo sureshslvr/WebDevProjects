601ef8b923bc6f2e9607e6edcbfb4426bb9a113a
suresh siliveru <sureshslvr@gmail.com>
2025-11-06 05:55:09 +0530

    Node.js, Data Types, and Core Concepts

--- Tasks/javascript/example.js
+++ Tasks/javascript/example.js
@@ -1,5 +1,24 @@
-console.log("welcome");
-console.error("this is error");
-console.warn("this is warning");
+const user={
+    firstName:'vijay',
+    age:24,
+    city:'hyderabad'
+  }
+  user.firstName='suresh';
+  console.log(user.firstName);
 
-alert("welocome to the new page");
+
+  let heros=["a","b","c"];
+  //index start with zero
+  console.log(heros[0])
+  console.log(heros.length)
+  console.log(heros[heros.length-1])
+
+let isValue = '123';
+console.log(typeof Number(isValue));  // number
+console.log(typeof isValue);          // string
+
+let a=1;
+let b="1";
+console.log(a==b)//true
+console.log(a===b)//false
+
